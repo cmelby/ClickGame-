53b845de1239f50cabffaf2957004a5cea6e03a3
Christopher Melby <melby.christopher@gmail.com>
2020-02-06 09:58:25 -0800

    adding image cards to json, finishing up app.js functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Characters from "./Characters.json";
+import CharacterCard from "./components/CharacterCard";
 import './App.css';
 
-function App() {
+
+class App extends Component {
+
+state = {
+  Characters
+}
+removeCharacter = id => {
+  // Filter this.state.friends for friends with an id not equal to the id being removed
+  const Characters = this.state.Characters.filter(character => character.id !== id);
+  // Set this.state.friends equal to the new friends array
+  this.setState({ Characters });
+};
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+    <Title>Click A Character!</Title>
+    {this.state.Characters.map(character => (
+      <CharacterCard
+      removeCharacter={this.removeFriend}
+        id={character.id}
+        key={character.id}
+        name={character.name}
+        image={character.image}
+        occupation={character.occupation}
+        location={character.location}
+      />
+    ))}
+  </Wrapper>
   );
 }
 
+}
 export default App;
+
