3b70b4e1931c352feccdd3e17286eeb12f1c3234
Christopher Melby <melby.christopher@gmail.com>
2020-02-06 18:34:42 -0800

    nav component built

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import Characters from "./Characters.json";
 import CharacterCard from "./components/CharacterCard";
+import Navbar from "./components/Navbar";
 import './App.css';
 
 
@@ -12,25 +13,26 @@ state = {
   Characters
 }
 removeCharacter = id => {
-  // Filter this.state.friends for friends with an id not equal to the id being removed
   const Characters = this.state.Characters.filter(character => character.id !== id);
-  // Set this.state.friends equal to the new friends array
   this.setState({ Characters });
 };
+
 
 render() {
   return (
     <Wrapper>
-    <Title>Click A Character!</Title>
+      
+      <Navbar>
+      </Navbar>
+   
+    <Title>Don't Click The Same Character!</Title>
     {this.state.Characters.map(character => (
       <CharacterCard
       removeCharacter={this.removeFriend}
         id={character.id}
         key={character.id}
-        name={character.name}
         image={character.image}
-        occupation={character.occupation}
-        location={character.location}
+
       />
     ))}
   </Wrapper>
--- my-app/src/components/CharacterCard/index.js
+++ my-app/src/components/CharacterCard/index.js
@@ -4,25 +4,11 @@ import "./style.css";
 function CharacterCard(props) {
   return (
     <div className="card">
-      <div className="img-container">
+   <div className="img-container">
         <img alt={props.name} src={props.image} />
+
+      <span onClick={() => props.removeFriend(props.id)} className="remove"></span>
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
     </div>
   );
 }
